54302624b75cf55892bd7177f37c00504480ca37
StevenMcElligott <89483932+StevenMcElligott@users.noreply.github.com>
2023-03-07 23:28:47 -0500

    Revert "clean up Docs sidebar"
    This reverts commit 058ffd183a710d62cae157569a8b49524dc58b3a.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,10 +35,14 @@ const config = {
   presets: [
     [
       'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        // https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-docs#configuration
         docs: {
-          sidebarItemsGenerator: require('./sidebar.js'),
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
--- sidebars.js
+++ sidebars.js
@@ -30,15 +30,4 @@ const sidebars = {
    */
 };
 
-function skipIndex(items) {
-  return items.filter(({ type, id }) => {
-    return type !== 'doc' || id !== 'index';
-  });
-}
-
-module.exports = async function sidebarItemsGenerator({ defaultSidebarItemsGenerator, ...args }) {
-  const sidebarItems = await defaultSidebarItemsGenerator(args);
-  return skipIndex(sidebarItems);
-}
-
 module.exports = sidebars;

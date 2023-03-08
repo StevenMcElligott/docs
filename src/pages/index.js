b6334767a23bea6503cc31bbecbe166802a25ca9
StevenMcElligott <89483932+StevenMcElligott@users.noreply.github.com>
2023-03-08 00:06:48 -0500

    fix index

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,12 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Docs
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/blog">
+            Docs
           </Link>
         </div>
       </div>
@@ -30,8 +35,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="My HomeLab Docs Site <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />

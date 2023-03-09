6049adb848716b5eae330db52bfdd572d9ee471e
StevenMcElligott <89483932+StevenMcElligott@users.noreply.github.com>
2023-03-09 00:38:17 -0500

    fix edit button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/stevenmcelligott/docs/',
+            'https://github.com/StevenMcElligott/docs/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/stevenmcelligott/docs/',
+            'https://github.com/StevenMcElligott/docs/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
